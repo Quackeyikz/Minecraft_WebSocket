90439cae72979a66461afa5c39423a56b34f370f
!rez <dimasrezaa117@gmail.com>
2025-12-04 09:42:17 +0700

    (feat): server chat and server log

--- src/server.py
+++ src/server.py
@@ -15,6 +15,7 @@ async def handle(ws):
             username = f"Player_{random.randint(1,99)}"
         usernames[ws] = username
         
+        print(f"+ {usernames[ws]} joined.")
         for c in clients:
             await c.send(f"+ {usernames[ws]} joined.")
                 
@@ -23,9 +24,9 @@ async def handle(ws):
                 newName = msg[10:].strip()
                 oldName = usernames[ws]
                 usernames[ws] = newName
-
+                
                 broadcast_msg = f"* {oldName} is now known as {newName}"
-            
+                print(broadcast_msg)
                 for c in clients:
                     await c.send(broadcast_msg)
                 continue
@@ -33,17 +34,32 @@ async def handle(ws):
             for c in clients:
                 full_msg = f"<{usernames[ws]}> {msg}"
                 await c.send(full_msg)
+            print (full_msg)
     finally:
         clients.remove(ws)
-        
+        print(f"- {usernames[ws]} leave.")
         for c in clients:
             await c.send(f"- {usernames[ws]} leave.")
 
         del usernames[ws]
 
+async def server_chat():
+    loop= asyncio.get_event_loop()
+    while True:
+        msg=await loop.run_in_executor(None,input,"> ")
+        broadcast_msg = f"[Server] {msg}"
+        print(broadcast_msg)
+        for c in clients:
+            try:
+                await c.send
+            except:
+                pass
 async def main():
     async with websockets.serve(handle, "0.0.0.0", 8765):
         print("Server running on ws://0.0.0.0:8765")
-        await asyncio.Future()  # Run forever
-
+        
+        await asyncio.gather(
+            asyncio.Future(),# Run forever
+            server_chat
+        )
 asyncio.run(main())
